3ac915a935add90abce6dcc29bafb8ecf166073e
noraneko <masima206@gmail.com>
2026-04-01 14:10:41 +0900

    fixed animTree behaviour, added jump and gravity, synchronized animTree

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,12 +2,14 @@ extends CharacterBody3D
 
 @export var speed := 5.0
 @export var mouse_sensitivity := 0.002
+@export var jump_velocity = 30
 
 @onready var name_label = $Label3D
 @onready var camera = $Camera3D
 @onready var anim = $AnimationTree
 
 var rotation_x := 0.0
+var input_dir = Vector2.ZERO
 
 var is_local = true
 
@@ -27,18 +29,21 @@ func _unhandled_input(event):
 
 
 func send_state():
-	update_state.rpc(global_position, rotation)
+	update_state.rpc(global_position, rotation, input_dir)
 
 @rpc("unreliable")
-func update_state(pos, rot):
+func update_state(pos, rot, in_dir):
 	if not is_multiplayer_authority():
 		global_position = pos
 		rotation = rot
+		input_dir = in_dir
+		handle_animation()
 
 func _physics_process(delta):
 	if is_multiplayer_authority():
 		process_input()
 		send_state()
+		apply_gravity(delta)
 
 
 func process_input():
@@ -56,6 +61,8 @@ func process_input():
 	if Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
 
+	if Input.is_action_just_pressed("move_jump"):
+		jump()
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE else Input.MOUSE_MODE_VISIBLE)
@@ -64,8 +71,19 @@ func process_input():
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
 	
-	anim.set("parameters/conditions/moving", direction != Vector3.ZERO)
-	anim.set("parameters/conditions/idle", direction == Vector3.ZERO)
-	anim.set("parameters/BlendSpace2D/blend_position", Vector2(direction.x, -direction.z))
-	
+	input_dir = Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
+	handle_animation()
 	move_and_slide()
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+func jump():
+	if is_on_floor():
+		velocity.y += jump_velocity
+
+func handle_animation():
+	anim.set("parameters/conditions/moving", input_dir != Vector2.ZERO)
+	anim.set("parameters/conditions/idle", input_dir == Vector2.ZERO)
+	anim.set("parameters/BlendSpace2D/blend_position", input_dir)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,15 +1,20 @@
 extends Control
 
 @export var level:String = "res://scenes/test_world.tscn"
+var ip = "127.0.0.1"
 
 func _on_host_pressed():
 	Lobby.create_game()
 	get_tree().change_scene_to_file(level)
 
 func _on_join_pressed():
-	Lobby.join_game()
+	Lobby.join_game(ip)
 	get_tree().change_scene_to_file(level)
 
 
 func _on_line_edit_text_changed(new_text: String) -> void:
 	Lobby.set_playername(new_text)
+
+
+func _on_ip_edit_text_changed(new_text: String) -> void:
+	ip = new_text
