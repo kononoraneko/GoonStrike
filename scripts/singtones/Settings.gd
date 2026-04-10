455b94ef5ab835a8889b9ed6a616b5c6476b1d04
noraneko <masima206@gmail.com>
2026-04-10 13:44:32 +0900

    some little changes

--- scripts/singtones/Settings.gd
+++ scripts/singtones/Settings.gd
@@ -6,3 +6,13 @@ extends Node
 		potato_changed.emit(val)
 
 signal potato_changed(val)
+
+func _ready() -> void:
+	connect("potato_changed", _on_potato_changed)
+
+func _on_potato_changed() -> void:
+	if potato:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+		DisplayServer.window_set_size(Vector2i(640,320))
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ADAPTIVE)
