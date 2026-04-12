5a8d8cff72a159bbd491f24d80eb4dfb676a1ac2
noraneko <masima206@gmail.com>
2026-04-12 22:24:55 +0900

    edited poses

--- scripts/autoloads/Settings.gd
+++ scripts/autoloads/Settings.gd
@@ -9,8 +9,8 @@ signal potato_changed(val)
 func _ready() -> void:
 	connect("potato_changed", _on_potato_changed)
 
-func _on_potato_changed() -> void:
-	if potato:
+func _on_potato_changed(val) -> void:
+	if val:
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 		DisplayServer.window_set_size(Vector2i(640,320))
 	else:
